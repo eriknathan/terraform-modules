8191c67b34553b626331953484a40d1f85193384
eriknathan <eriknathan.contato@gmail.com>
2025-04-08 13:52:41 -0300

    first commit

---
+++ modules/ec2-instance/variables.tf
@@ -0,0 +1,30 @@
+variable "project_name" {
+  type        = string
+  description = "Project Name to be used to name the resources (tags)"
+}
+
+variable "tags" {
+  type        = map(any)
+  description = "Tags to be added to AWS Resources"
+}
+
+variable "ami_id" {
+  type        = string
+  description = "AMI ID da instância EC2"
+}
+
+variable "instance_type" {
+  type        = string
+  description = "Tipo da instância EC2"
+  default     = "t2.micro"
+}
+
+variable "subnet_id" {
+  type        = string
+  description = "ID da subnet onde a instância será lançada"
+}
+
+variable "instance_name" {
+  type        = string
+  description = "Nome da instância (tag)"
+}
